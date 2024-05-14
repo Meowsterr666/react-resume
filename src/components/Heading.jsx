8a0a7f77a2a5345ab97f2fbbe39014eacf4d3835
Abel Pineda <iforgotmy666@icloud.com>
2024-05-13 20:44:04 -0700

    fix: img file structure

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -10,7 +10,7 @@ function Heading() {
         <h1>Hello!</h1>
         <div className="glitch">
           <h1 title="I'm Abel Pineda">I&#39;m Abel Pineda</h1>
-          <img src="./images/132092486.png"></img>
+          <img src="./../images/132092486.png"></img>
         </div>
         <div className="summary-box">
           <h1>Summary</h1>
