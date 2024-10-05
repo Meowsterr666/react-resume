d1a21ba530b05b8b6d936c4355c8107596a89731
Abel Pineda <iforgotmy666@icloud.com>
2024-10-04 22:46:32 -0700

    modify words and awards

--- src/components/sub-components/Awards.jsx
+++ src/components/sub-components/Awards.jsx
@@ -19,8 +19,15 @@ function Award() {
         <p>
           Codecademy (Learn Python 3 Course) - 2023
         </p>
+        <p>
+          Tryhackme (Pre Security) - 24th September 2024
+        </p>
     </div>
   );
 }
 
 export default Award;
+
+// git remote add origin https://github.com/Meowsterr666/sad.git
+// git branch -M main
+// git push -u origin main
--- src/components/sub-components/Info.jsx
+++ src/components/sub-components/Info.jsx
@@ -3,15 +3,12 @@ function Info() {
       <div>
         {/* <a href="#" className="hobby">Hobbies</a> */}
         <details className="detail-whole">
-          <summary id="detail-container">Contact Me</summary>
+          <summary id="detail-container">Contact</summary>
             <p className="detail-item">
               Soledad, Ca, 93960
             </p>
             <p className="detail-item">
               abel93alejandroftl@gmail.com
-            </p>
-            <p className="detail-item">
-              (831)-970-2327
             </p>
         </details>
       </div>
